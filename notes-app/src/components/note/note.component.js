22ab4a6353d5aba94aa01077acb4e9f7394898c7
Jarosław Kudzia <jaroslawkudzia73@gmail.com>
2022-03-07 21:25:57 +0100

    feat: added links on dates and delete action

--- notes-app/src/components/note/note.component.js
+++ notes-app/src/components/note/note.component.js
@@ -1,21 +1,32 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import ReactMarkdown from 'react-markdown';
 
-const NoteComponent = ({ note }) => {
-  const { noteText, noteDate } = note;
+const NoteComponent = ({ note, deleteNote }) => {
+	const { noteText, noteDate, noteId } = note;
 
-  // TODO: add router link to date
-  return (
-    <div className="flex flex-col sm:flex-row w-full my-4 bg-gray-200 p-3">
-      <div className="flex flex-col w-full sm:s-4/5 space-y-2">
-        <span className="flex text-sm w-full h-16 text-ellipsis overflow-hidden"
-        style={{"display":"-webkit-box","-webkit-line-clamp": "3", "-webkit-box-orient": "vertical"}}>{noteText}</span>
-        <span className="text-sm text-gray-500">{noteDate}</span>
-      </div>
-      <div className="flex w-full sm:w-1/5 justify-center items-center">
-        <button className="bg-red-400 text-white text-sm p-2 rounded-sm">Delete note</button>
-      </div>
-    </div>
-  )
-}
+	return (
+		<div className='flex flex-col sm:flex-row w-full my-4 bg-gray-200 p-3 rounded-sm'>
+			<div className='flex flex-col w-full sm:s-4/5 space-y-2'>
+				<span className='flex text-sm w-full h-16 text-ellipsis overflow-hidden  prose' style={{ display: '-webkit-box', WebkitLineClamp: '3', WebkitBoxOrient: 'vertical' }}>
+					<ReactMarkdown>{noteText}</ReactMarkdown>
+				</span>
 
-export default NoteComponent
+				<Link to={'/note/' + noteId}>
+					<span className='text-sm text-gray-500'>{noteDate}</span>
+				</Link>
+			</div>
+			<div className='flex w-full sm:w-1/5 justify-center items-center'>
+				<button
+					className='bg-red-400 text-white text-sm p-2 rounded-sm'
+					onClick={() => {
+						deleteNote(noteId);
+					}}>
+					Delete note
+				</button>
+			</div>
+		</div>
+	);
+};
+
+export default NoteComponent;
